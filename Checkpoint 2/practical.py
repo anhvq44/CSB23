0dba84be6c1ad019a614799f8b2dc1c979566fb3
anhvq44 <quet31011120@gmail.com>
2025-07-27 10:58:54 +0700

    checkpoint2

---
+++ Checkpoint 2/practical.py
@@ -0,0 +1,27 @@
+def CombineArrays(nums1, nums2):
+    res = [0]*(len(nums1) + len(nums2))
+    i = 0
+    j = 0
+    k = 0
+    
+    while (i<len(nums1)) and (j<len(nums2)):
+        if nums2[j] < nums1[i]:
+            res[k] = nums2[j]
+            j += 1
+        else:
+            res[k] = nums1[i]
+            i += 1
+        k += 1
+    
+    while i<len(nums1):
+        res[k] = nums1[i]
+        i += 1
+        k += 1
+    while j<len(nums2):
+        res[k] = nums2[j]
+        j += 1
+        k += 1
+    
+    return res
+
+print(CombineArrays([2, 5, 6], [1, 2, 3]))
