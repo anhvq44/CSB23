4e63a0b7d120d99d2d41de7453379c87b610bdc3
anhvq44 <quet31011120@gmail.com>
2025-08-10 10:08:14 +0700

    upd hw10

--- Lesson10/hw10/hw10.py
+++ Lesson10/hw10/hw10.py
@@ -1,27 +1,33 @@
 import csv
 
-email_list = set()
-company_counts = {}
 
-with open("Lesson10\hw10\data.csv", 'r') as file:
-    csvreader = csv.reader(file)
+def process_csv(file_path):
+    email_list = set()
+    company_counts = {}
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        csvreader = csv.reader(file)
     
-    for row in csvreader:
-        name, company, email = row
-        email = email.strip().lower()
+        for row in csvreader:
+            if len(row) != 3:
+                continue
+            
+            name, company, email = row
+            email = email.strip().lower()
+            company = company.strip()
         
-        if email not in email_list:
-            email_list.add(email)
+            if email not in email_list:
+                email_list.add(email)
 
-            if company not in company_counts:
-                company_counts[company] = 1
-            else:
-                company_counts[company]+=1
-            
+                if company not in company_counts:
+                    company_counts[company] = 1
+                else:
+                    company_counts[company]+=1
+    sorted_company_list = sorted(company_counts.items(), key=lambda x: x[1], reverse=True)
 
-            
-print(f'Số lượng đại biểu dự kiến tham dự: {len(email_list)}')
-sorted_company_list = sorted(company_counts.items(), key=lambda x: x[1], reverse=True)
-
-for company, num_people in sorted_company_list:
-    print(f'{company}: {num_people}')
+    print(f'Số lượng đại biểu dự kiến tham dự: {len(email_list)}')
+    print("Danh sách công ty và số lượng đại biểu:")
+    for company, num_people in sorted_company_list:
+        print(f'{company}: {num_people}')
+        
+process_csv("Lesson10/hw10/data.csv")
